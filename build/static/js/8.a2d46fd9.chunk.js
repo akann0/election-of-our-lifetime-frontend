cfa5845c137ffb056a19c8d3069480c583aae7d1
akann0 <akann@ucsd.edu>
2025-08-24 08:39:52 -0700

    deploy build

---
+++ build/static/js/8.a2d46fd9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[8],{8:(e,n,s)=>{s.r(n),s.d(n,{default:()=>t});const t={}}}]);
+//# sourceMappingURL=8.a2d46fd9.chunk.js.map
